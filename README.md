5c82a9e6ff2d3e428eed1db86964e704a17f531f
shxd <paolaccialesssndro@gmail.com>
2026-07-23 19:54:04 +0200

    did UVs

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-A program to record MKWii Time Trials without an emulator
+A program to record MKWii Time Trials without an emulator.
+
+> [!NOTE]
+> Some BRRES rendering/parsing logic was inspired by [Riistudio](https://github.com/snailspeed3/RiiStudio) and [noclip.website](https://github.com/magcius/noclip.website/).
 
 ## Prerequisites
-
-This project depends on the `brres` crate, which requires LLVM 19.1.7 to build correctly (or at least that's the version i tested) <br/>
+This project is in Rust and uses the Bevy game engine. It also depends on the `brres` crate, which requires LLVM 19.1.7 to build correctly (or at least that's the version i tested) <br/>
 on windows, install the binary [here](https://github.com/llvm/llvm-project/releases/tag/llvmorg-19.1.7)
 
 make sure to create `.cargo/config.toml` and put your paths or change the linker toolchain
 
-### todo:
-- brres renderer
-- UI
-- kinoko integration
-- camera movement
-- events that could be hooked up to gui/audio
-- mp4 exporting
-- input rendering
-- top 10 rendering
-- kcl/kmp rendering
-- rewrite of szs and brres crates
+### TODOs:
+  - [ ] brres renderer (current)
+  - [ ] Kinoko integration (crucial)
+  - [ ] UI
+  - [ ] camera movement
+  - [ ] mp4 exporting
+  - [ ] input rendering
+  - [ ] top 10 rendering
+  - [ ] stuff like speedometer and custom texts
+  - [ ] kcl/kmp rendering
+  - [ ] rewrite of szs and brres crates (very late in the project, not needed)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::mesh::Indices;
 use bevy::{asset::RenderAssetUsages, prelude::*, render::render_resource::PrimitiveTopology};
-use brres::{MatrixPrimitive, VertexNormalBuffer, VertexPositionBuffer, };
 use brres::Mesh as BrresMesh;
+use brres::{MatrixPrimitive, VertexNormalBuffer, VertexPositionBuffer};
 
 // example brought down to the essentials from here: https://bevy.org/examples/3d-rendering/generate-custom-mesh/
 
@@ -15,7 +15,6 @@ fn main() {
         .add_systems(Update, camera_movement)
         .run();
 }
-
 
 // https://github.com/snailspeed3/RiiStudio/blob/master/source/librii/gx/Vertex.hpp#L155
 pub enum PrimitiveType {
@@ -58,60 +57,160 @@ impl PrimitiveType {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum GXAttr {
+    PNMTXIDX = 0,
+    TEX0MTXIDX = 1,
+    TEX1MTXIDX = 2,
+    TEX2MTXIDX = 3,
+    TEX3MTXIDX = 4,
+    TEX4MTXIDX = 5,
+    TEX5MTXIDX = 6,
+    TEX6MTXIDX = 7,
+    TEX7MTXIDX = 8,
+    POS = 9,
+    NRM = 10,
+    CLR0 = 11,
+    CLR1 = 12,
+    TEX0 = 13,
+    TEX1 = 14,
+    TEX2 = 15,
+    TEX3 = 16,
+    TEX4 = 17,
+    TEX5 = 18,
+    TEX6 = 19,
+    TEX7 = 20,
+}
+
+impl GXAttr {
+    pub fn has_attribute(vcd: i32, attr: &Self) -> bool {
+        (vcd & (1 << *attr as i32)) != 0
+    }
+}
+
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
+    mut bevy_meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let archive = brres::Archive::from_path("test/course_model.brres").unwrap();
 
     let models = &archive.models;
 
-    let pos_bufs: Vec<VertexPositionBuffer> = models
-        .iter()
-        .flat_map(|model| model.positions.clone())
-        .collect();
+    let mut global_mesh_idx = 0;
+    for model in models.iter() {
+        let name = &model.name;
+        let info = &model.info;
 
-    let nrm_bufs: Vec<VertexNormalBuffer> = models
-        .iter()
-        .flat_map(|model| model.normals.clone())
-        .collect();
+        let bones = &model.bones;
+        let mats = &model.materials;
+        let meshes = &model.meshes;
 
-    let brres_meshes: Vec<BrresMesh> = models
-        .iter()
-        .flat_map(|model| model.meshes.clone())
-        .collect();
+        let pos_bufs = &model.positions;
+        let nrm_bufs = &model.normals;
+        let uv_bufs = &model.texcoords;
+        let clr_bufs = &model.colors;
 
-    // first mesh for now
-    let mesh = &brres_meshes[0];
+        let matrices = &model.matrices;
 
-    let primitives = decode_matrix_primitive(&mesh);
+        for mesh in meshes.iter() {
+            let mesh_idx = global_mesh_idx;
+            global_mesh_idx += 1;
 
-    let mut positions = Vec::new();
-    let mut normals = Vec::new();
-    let mut indices = Vec::new();
+            let primitives = decode_matrix_primitive(mesh);
+            let (vertices, indices) = build_mesh_topology(&primitives);
 
-    for prim in primitives {
-        println!("{:02x}", prim.prim_type.to_hex());
-        // handle primitive types
+            let has_pos = GXAttr::has_attribute(mesh.vcd, &GXAttr::POS);
+            let has_nrm = GXAttr::has_attribute(mesh.vcd, &GXAttr::NRM);
+            let has_uv = GXAttr::has_attribute(mesh.vcd, &GXAttr::TEX0);
+
+            let pos_buf = if has_pos {
+                match pos_bufs.iter().find(|buf| buf.name == mesh.pos_buffer) {
+                    Some(buf) => buf,
+                    None => {
+                        println!("Missing position buffer: {}", mesh.pos_buffer);
+                        continue;
+                    }
+                }
+            } else {
+                println!("Mesh has no position attribute: {}", mesh.name);
+                continue;
+            };
+
+            let nrm_buf = {
+                if has_nrm {
+                    nrm_bufs.iter().find(|buf| buf.name == mesh.nrm_buffer)
+                } else {
+                    None
+                }
+            };
+
+            // first channel for now
+            let uv_buf = if has_uv {
+                mesh.uv_buffer
+                    .first()
+                    .and_then(|name| uv_bufs.iter().find(|buf| buf.name == *name))
+            } else {
+                None
+            };
+
+            let vcd = mesh.vcd;
+
+            let mut positions = Vec::with_capacity(vertices.len());
+            
+            let mut normals = if has_nrm {
+                Some(Vec::with_capacity(vertices.len()))
+            } else {
+                None
+            };
+            
+            let mut uvs = if has_uv {
+                Some(Vec::with_capacity(vertices.len()))
+            } else {
+                None
+            };
+
+            for vtx in &vertices {
+                let pos_index = vtx.indices[GXAttr::POS as usize] as usize;
+                positions.push(pos_buf.data[pos_index]);
+            
+                if let Some(normals) = &mut normals {
+                    let nrm_index = vtx.indices[GXAttr::NRM as usize] as usize;
+                    normals.push(nrm_buf.unwrap().data[nrm_index]);
+                }
+            
+                if let Some(uvs) = &mut uvs {
+                    let uv_index = vtx.indices[GXAttr::TEX0 as usize] as usize;
+                    uvs.push(uv_buf.unwrap().data[uv_index]);
+                }
+            }
+
+            println!(
+                "Mesh {mesh_idx}: GX vertices: {}, indices: {}, positions: {}, normals: {}, uvs: {}",
+                vertices.len(),
+                indices.len(),
+                positions.len(),
+                if let Some(normals) = &normals { normals.len() } else { 0 },
+                if let Some(uvs) = &uvs { uvs.len() } else { 0 },
+            );
+
+            let mesh_handle = bevy_meshes.add(create_mesh(positions, normals, uvs, indices));
+
+            commands.spawn((
+                Mesh3d(mesh_handle),
+                MeshMaterial3d(materials.add(StandardMaterial {
+                    base_color: Color::srgba(1.0, 1.0, 1.0, 1.0),
+                    ..default()
+                })),
+            ));
+        }
     }
-
-    let mesh_handle = meshes.add(create_mesh(positions, normals, indices));
-
-    commands.spawn((
-        Mesh3d(mesh_handle),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgba(1.0, 1.0, 1.0, 1.0),
-            ..default()
-        })),
-    ));
 
     commands.spawn((Camera3d::default(), MainCamera));
     commands.spawn((
         PointLight::default(),
         Transform::from_xyz(1.0, 1.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
-    
 }
 
 #[derive(Clone)]
@@ -124,48 +223,139 @@ pub struct Primitive {
     pub vertices: Vec<IndexedVertex>,
 }
 
+fn build_mesh_topology(primitives: &[Primitive]) -> (Vec<IndexedVertex>, Vec<u32>) {
+    let mut vertices = Vec::<IndexedVertex>::new();
+    let mut indices = Vec::new();
+
+    for prim in primitives {
+        //println!("0x{:02x}", prim.prim_type.to_hex());
+        let offset = vertices.len() as u32;
+        vertices.extend(prim.vertices.clone());
+
+        match prim.prim_type {
+            PrimitiveType::Quads | PrimitiveType::Quads2 => {
+                for i in (0..prim.vertices.len()).step_by(4) {
+                    let v0 = offset + i as u32;
+                    let v1 = offset + i as u32 + 1;
+                    let v2 = offset + i as u32 + 2;
+                    let v3 = offset + i as u32 + 3;
+
+                    indices.push(v0);
+                    indices.push(v1);
+                    indices.push(v2);
+
+                    indices.push(v0);
+                    indices.push(v2);
+                    indices.push(v3);
+                }
+            }
+            PrimitiveType::Triangles => {
+                for i in 0..prim.vertices.len() {
+                    indices.push(offset + i as u32);
+                }
+            }
+            PrimitiveType::TriangleStrip => {
+                for i in 2..prim.vertices.len() {
+                    let v0 = offset + i as u32 - 2;
+                    let v1 = offset + i as u32 - ((!i as u32) & 1);
+                    let v2 = offset + i as u32 - ((i as u32) & 1);
+
+                    indices.push(v0);
+                    indices.push(v1);
+                    indices.push(v2);
+                }
+            }
+            PrimitiveType::TriangleFan => {
+                for i in 2..prim.vertices.len() {
+                    let v0 = offset;
+                    let v1 = offset + i as u32 - 1;
+                    let v2 = offset + i as u32;
+
+                    indices.push(v0);
+                    indices.push(v1);
+                    indices.push(v2);
+                }
+            }
+            _ => {
+                println!(
+                    "Unsupported primitive (i don't render Lines, LineStrip, and Points): {:?}",
+                    prim.prim_type.to_hex()
+                );
+            }
+        }
+    }
+
+    (vertices, indices)
+}
+
 // function translated and modified to rust from the python example (while returning what i need)
 fn decode_matrix_primitive(mesh: &BrresMesh) -> Vec<Primitive> {
     let mut primitives = Vec::new();
-    
+
     for mprim in &mesh.mprims {
         let buf = &mprim.vertex_data_buffer;
         let mut cursor = 0;
-        
+
         while cursor + 4 <= buf.len() {
             let prim_type = buf[cursor];
             let prim_vtx_count = ((buf[cursor + 1] as u32) << 16)
                 | ((buf[cursor + 2] as u32) << 8)
                 | (buf[cursor + 3] as u32);
             cursor += 4;
-            
+
             if prim_vtx_count == 0 || cursor + (prim_vtx_count as usize * 52) > buf.len() {
                 break;
             }
-            
+
             let mut vertices = Vec::new();
-            
+
             for _ in 0..prim_vtx_count {
                 let mut indices = [0u16; 26];
                 for i in 0..26 {
-                    indices[i] = (buf[cursor] as u16) | ((buf[cursor + 1] as u16) << 8);
+                    indices[i] = ((buf[cursor] as u16) | ((buf[cursor + 1] as u16) << 8));
                     cursor += 2;
                 }
                 vertices.push(IndexedVertex { indices });
             }
-            
-            primitives.push(Primitive { prim_type: PrimitiveType::from_u8(prim_type), vertices });
+
+            primitives.push(Primitive {
+                prim_type: PrimitiveType::from_u8(prim_type),
+                vertices,
+            });
         }
     }
-    
+
     primitives
 }
 
-fn create_mesh(pos: Vec<[f32; 3]>, nrm: Vec<[f32; 3]>, idx: Vec<u32>) -> Mesh {
-    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default())
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, pos)
-        .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, nrm)
-        .with_inserted_indices(Indices::U32(idx))
+fn create_mesh(pos: Vec<[f32; 3]>, nrm: Option<Vec<[f32; 3]>>, uvs: Option<Vec<[f32; 2]>>, indices: Vec<u32>) -> Mesh {
+    let mut mesh = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::default(),
+    );
+
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        pos,
+    );
+
+    if let Some(normals) = nrm {
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            normals,
+        );
+    }
+
+    if let Some(uvs) = uvs {
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_UV_0,
+            uvs,
+        );
+    }
+
+    mesh.insert_indices(Indices::U32(indices));
+
+    mesh
 }
 
 fn camera_movement(
